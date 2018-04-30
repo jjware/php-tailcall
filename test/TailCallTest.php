9e8434a86ae04be3f53298924245421dffa90a10
Joshua Jones <joshua.jones.software@gmail.com>
2018-04-29 20:53:17 -0400

    add test files

---
+++ test/TailCallTest.php
@@ -0,0 +1,13 @@
+<?php
+declare(strict_types=1);
+
+use JJWare\Utils\TailCall\TailCall;
+use JJWare\Utils\TailCall\Ret;
+use JJWare\Utils\TailCall\Sus;
+
+use PHPUnit\Framework\TestCase;
+
+class TailCallTest extends TestCase
+{
+
+}
